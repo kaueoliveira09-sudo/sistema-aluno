f5959e35c36b868bdf019eaf3b70a176a6163ca0
kaueoliveira09-sudo <kaue.oliveira09@escola.pr.gov.br>
2026-03-16 11:52:59 -0300

    Add files via upload

---
+++ node.app.js
@@ -0,0 +1,6 @@
+const nomeAluno = "João Silva"; // Variável String
+const idade = 21;              // Variável Number
+const matriculado = true;       // Variável Boolean
+
+// Este é o console detalhado da branch feature/mensagem-detalhada
+console.log(`Relatório do Sistema: O aluno ${nomeAluno} (Idade: ${idade}) está atualmente ${matriculado ? "Ativo" : "Inativo"} no sistema.`);
